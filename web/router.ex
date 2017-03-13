a16b9811b8653b87db4792b82a490b5db0fad214
HP <HP@yahoo>
2017-03-14 05:20:47 +0700

    add a nested resource

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,10 @@ defmodule HelloPhoenix.Router do
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
 
-    resources "/users", UserController
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+
     resources "/posts", PostController, only: [:index, :show]
     resources "/comments", CommentController, except: [:delete]
   end
